4441b90002d449737ff15b941b788c9f0b9ea211
titan717 <nichus973@gmail.com>
2026-09-25 22:47:39 +0530

    feat(videos): integrate Phase 6 video routes and version

--- src/providers/tmdb/index.ts
+++ src/providers/tmdb/index.ts
@@ -1,4 +1,8 @@
 export { TmdbClient, TmdbProviderError } from "./client.js";
+export { TmdbVideoClient } from "./videos.js";
 export { createTmdbRoutes } from "./routes.js";
+export { createTmdbVideoRoutes } from "./videos-routes.js";
 export { normalizeTmdbMovie, normalizeTmdbTv, normalizeTmdbMovieResult, normalizeTmdbTvResult, normalizeTmdbTrendingResult, tmdbImage } from "./normalizer.js";
+export { normalizeTmdbVideo, selectPrimaryTrailer } from "./video-normalizer.js";
 export * from "./schemas.js";
+export * from "./video-schemas.js";
